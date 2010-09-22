bda9204e492d415d10c66c0149539f70f0c54d2e
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-22 16:21:57 +0200

    Commented out IPN code

--- satchmo_ogone/payment/modules/ogone/views.py
+++ satchmo_ogone/payment/modules/ogone/views.py
@@ -109,93 +109,93 @@ def confirm_info(request):
     return render_to_response(template, context_instance=ctx)
 confirm_info = never_cache(confirm_info)
 
-@csrf_exempt
-def ipn(request):
-    """Ogone IPN (Instant Payment Notification)
-    Cornfirms that payment has been completed and marks invoice as paid.
-    Adapted from IPN cgi script provided at http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/456361"""
-    payment_module = config_get_group('PAYMENT_OGONE')
-    if payment_module.LIVE.value:
-        log.debug("Live IPN on %s", payment_module.KEY.value)
-        url = payment_module.POST_URL.value
-        account = payment_module.BUSINESS.value
-    else:
-        log.debug("Test IPN on %s", payment_module.KEY.value)
-        url = payment_module.POST_TEST_URL.value
-        account = payment_module.BUSINESS_TEST.value
-    PP_URL = url
-
-    try:
-        data = request.POST
-        log.debug("Ogone IPN data: " + repr(data))
-        if not confirm_ipn_data(data, PP_URL):
-            return HttpResponse()
-
-        if not 'payment_status' in data or not data['payment_status'] == "Completed":
-            # We want to respond to anything that isn't a payment - but we won't insert into our database.
-             log.info("Ignoring IPN data for non-completed payment.")
-             return HttpResponse()
-
-        try:
-            invoice = data['invoice']
-        except:
-            invoice = data['item_number']
-
-        gross = data['mc_gross']
-        txn_id = data['txn_id']
-
-        if not OrderPayment.objects.filter(transaction_id=txn_id).count():
-            # If the payment hasn't already been processed:
-            order = Order.objects.get(pk=invoice)
-
-            order.add_status(status='New', notes=_("Paid through Ogone."))
-            processor = get_processor_by_key('PAYMENT_OGONE')
-            payment = processor.record_payment(order=order, amount=gross, transaction_id=txn_id)
-
-            if 'memo' in data:
-                if order.notes:
-                    notes = order.notes + "\n"
-                else:
-                    notes = ""
-
-                order.notes = notes + _('---Comment via Ogone IPN---') + u'\n' + data['memo']
-                order.save()
-                log.debug("Saved order notes from Ogone")
-
-            # Run only if subscription products are installed
-            if 'product.modules.subscription' in settings.INSTALLED_APPS:
-                for item in order.orderitem_set.filter(product__subscriptionproduct__recurring=True, completed=False):
-                    item.completed = True
-                    item.save()
-
-            for cart in Cart.objects.filter(customer=order.contact):
-                cart.empty()
-
-    except:
-        log.exception(''.join(format_exception(*exc_info())))
-
-    return HttpResponse()
-
-def confirm_ipn_data(data, PP_URL):
-    # data is the form data that was submitted to the IPN URL.
-
-    newparams = {}
-    for key in data.keys():
-        newparams[key] = data[key]
-
-    newparams['cmd'] = "_notify-validate"
-    params = urlencode(newparams)
-
-    req = urllib2.Request(PP_URL)
-    req.add_header("Content-type", "application/x-www-form-urlencoded")
-    fo = urllib2.urlopen(req, params)
-
-    ret = fo.read()
-    if ret == "VERIFIED":
-        log.info("Ogone IPN data verification was successful.")
-    else:
-        log.info("Ogone IPN data verification failed.")
-        log.debug("HTTP code %s, response text: '%s'" % (fo.code, ret))
-        return False
-
-    return True
+# @csrf_exempt
+# def ipn(request):
+#     """Ogone IPN (Instant Payment Notification)
+#     Cornfirms that payment has been completed and marks invoice as paid.
+#     Adapted from IPN cgi script provided at http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/456361"""
+#     payment_module = config_get_group('PAYMENT_OGONE')
+#     if payment_module.LIVE.value:
+#         log.debug("Live IPN on %s", payment_module.KEY.value)
+#         url = payment_module.POST_URL.value
+#         account = payment_module.BUSINESS.value
+#     else:
+#         log.debug("Test IPN on %s", payment_module.KEY.value)
+#         url = payment_module.POST_TEST_URL.value
+#         account = payment_module.BUSINESS_TEST.value
+#     PP_URL = url
+# 
+#     try:
+#         data = request.POST
+#         log.debug("Ogone IPN data: " + repr(data))
+#         if not confirm_ipn_data(data, PP_URL):
+#             return HttpResponse()
+# 
+#         if not 'payment_status' in data or not data['payment_status'] == "Completed":
+#             # We want to respond to anything that isn't a payment - but we won't insert into our database.
+#              log.info("Ignoring IPN data for non-completed payment.")
+#              return HttpResponse()
+# 
+#         try:
+#             invoice = data['invoice']
+#         except:
+#             invoice = data['item_number']
+# 
+#         gross = data['mc_gross']
+#         txn_id = data['txn_id']
+# 
+#         if not OrderPayment.objects.filter(transaction_id=txn_id).count():
+#             # If the payment hasn't already been processed:
+#             order = Order.objects.get(pk=invoice)
+# 
+#             order.add_status(status='New', notes=_("Paid through Ogone."))
+#             processor = get_processor_by_key('PAYMENT_OGONE')
+#             payment = processor.record_payment(order=order, amount=gross, transaction_id=txn_id)
+# 
+#             if 'memo' in data:
+#                 if order.notes:
+#                     notes = order.notes + "\n"
+#                 else:
+#                     notes = ""
+# 
+#                 order.notes = notes + _('---Comment via Ogone IPN---') + u'\n' + data['memo']
+#                 order.save()
+#                 log.debug("Saved order notes from Ogone")
+# 
+#             # Run only if subscription products are installed
+#             if 'product.modules.subscription' in settings.INSTALLED_APPS:
+#                 for item in order.orderitem_set.filter(product__subscriptionproduct__recurring=True, completed=False):
+#                     item.completed = True
+#                     item.save()
+# 
+#             for cart in Cart.objects.filter(customer=order.contact):
+#                 cart.empty()
+# 
+#     except:
+#         log.exception(''.join(format_exception(*exc_info())))
+# 
+#     return HttpResponse()
+# 
+# def confirm_ipn_data(data, PP_URL):
+#     # data is the form data that was submitted to the IPN URL.
+# 
+#     newparams = {}
+#     for key in data.keys():
+#         newparams[key] = data[key]
+# 
+#     newparams['cmd'] = "_notify-validate"
+#     params = urlencode(newparams)
+# 
+#     req = urllib2.Request(PP_URL)
+#     req.add_header("Content-type", "application/x-www-form-urlencoded")
+#     fo = urllib2.urlopen(req, params)
+# 
+#     ret = fo.read()
+#     if ret == "VERIFIED":
+#         log.info("Ogone IPN data verification was successful.")
+#     else:
+#         log.info("Ogone IPN data verification failed.")
+#         log.debug("HTTP code %s, response text: '%s'" % (fo.code, ret))
+#         return False
+# 
+#     return True
